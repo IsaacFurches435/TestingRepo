ac3ef40f06f2d45fb243549c8cb6f454c5a9216c
ShadowXC-code <shadowraider347@gmail.com>
2021-02-04 17:51:16 -0500

    Inital Commit

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,18 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+    
+    
+
+    public DriveTrain() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+}
